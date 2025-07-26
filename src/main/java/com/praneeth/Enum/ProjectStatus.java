e2ad5af5d800320e43431268d7d0fd8edf3789d8
Praneeth Katta <kattapraneeth288@gmail.com>
2025-07-27 00:29:48 +0530

    Add entities and enums.

---
+++ src/main/java/com/praneeth/Enum/ProjectStatus.java
@@ -0,0 +1,5 @@
+package com.praneeth.Enum;
+
+public enum ProjectStatus {
+    UPCOMING, IN_PROGRESS, COMPLETED
+}
---
+++ src/main/java/com/praneeth/Enum/UserRole.java
@@ -0,0 +1,5 @@
+package com.praneeth.Enum;
+
+public enum UserRole {
+    ADMIN, BUILDER, CLIENT
+}
